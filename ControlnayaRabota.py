22b311a5cecf51cab3c7f58e491c1adcfa6a6872
hp <you@exsample.com>
2024-06-08 20:49:34 +0500

    1 exam python

---
+++ ControlnayaRabota.py
@@ -0,0 +1,58 @@
+# 1.2)
+
+# class AutoSalon:
+#     car = 10
+#
+#     def __init__(self, type, age, lang):
+#         self.type = type
+#
+#     def __str__(self):
+#         self.age = age
+#         self.lang = lang
+#
+#         return f"{self.type}({self.age})({self.lang})}"
+#
+#
+# p = AutoSalon('Man', 40, 'UZB')
+# c = AutoSalon('WoMan', 20, 'RUSSIA')
+# print(c.type)
+# print(c.age)
+# print(c.lang)
+
+# 1.3
+# Инкапсулятция ооп это - процесс разделения элементов абстракций, определяющих её стурктуру
+
+
+# 2.1
+# def is_even(number):
+#     return number % 2 ==0
+#
+# # 2.3
+# def positive_numbers(numbers):
+#     return [num for num in numbers if num > 0]
+
+
+# numbers_list = [-1, 2, -3, 4, -5, 6]
+# positive_list = positive_numbers(numbers_list)
+# print(positive_list)
+
+# 2.5
+
+# class Student:
+#     def __init__(self, name):
+#         self.name = name
+
+
+#
+# # 3.1
+# def Diablo(d):
+#
+#     d = d.lower().replace(" ","")
+#     return d == d[::-1]
+#
+# d = "sdfghjklkjhgfdghjklkfjhgdhfjkdfdf"
+# print(Diablo(d))
+
+
+
+
